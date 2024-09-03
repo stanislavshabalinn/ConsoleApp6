02f364d7029d225c0dab6844906ffca01019b852
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-09-03 22:44:48 +0700

    повтор 3.3.1

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,13 +1,24 @@
-﻿namespace ConsoleApp6
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace ConsoleApp6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string MyName = "Stas";
+            byte Myage = 49;
+            bool HaveIApet = true;
+            double MуShoeSise = 42.5;
 
-            string Myname;
-            Myname = "Stas";
-            Console.WriteLine(Myname);
+
+            Console.WriteLine("My name is" + MyName);
+            Console.WriteLine("Myage"+ Myage);
+            Console.WriteLine("Do I have a pet?"+ HaveIApet);
+            Console.WriteLine("My shoe size is"+ MуShoeSise);
+            
             Console.ReadKey();
         }
     }
