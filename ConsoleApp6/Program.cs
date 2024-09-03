daae1f1f534067892d46f7506a0ab362977229e1
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-09-03 20:25:03 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Myname;
+            Myname = "Stas";
+            Console.WriteLine(Myname);
+            Console.ReadKey();
+        }
+    }
+}
