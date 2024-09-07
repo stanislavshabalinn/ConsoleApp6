62c9e14edf3b15aed0e24476fdc393492bce65ae
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-09-07 23:28:56 +0700

    картеж  повтор 4.4.3

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -5,17 +5,18 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        (string name, int age) anketa;
+        var (name, age) = ("Евгения", 27);
+
+        Console.WriteLine("Моё имя: {0}", name);
+        Console.WriteLine("Мой возраст: {0}", age);
 
         Console.Write("Введите имя: ");
-        anketa.name = Console.ReadLine();
-        Console.Write("Введите возраст с цифрами: ");
-        anketa.age = Convert.ToInt32(Console.ReadLine());
+        name = Console.ReadLine();
+        Console.Write("Введите возраст с цифрами:");
+        age = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Ваше имя: {0}", anketa.name);
-        Console.WriteLine("Ваш возраст: {0}", anketa.age);
-
-        Console.ReadKey();
+        Console.WriteLine("Ваше имя: {0}", name);
+        Console.WriteLine("Ваш возраст: {0}", age);
 
     }
 }
