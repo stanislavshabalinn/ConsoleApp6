7180d1c428f87cd0e9b429df2ec64171c35559e8
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-09-07 22:25:01 +0700

    повтор 4.3.12

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 
-namespace FirstApp
+class MainClass
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Введите свое имя");
-            var name = Console.ReadLine();
+        var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-            for (int i = name.Length - 1; i >= 0; i--)
-            { 
-                 Console.Write(name[i]+"");
+        int temp;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
             }
-            Console.ReadLine();
         }
 
+        foreach (var item in arr)
+        {
+            Console.Write(item);
+        }
     }
 }
