5bc8c1fa7b490973128a7819ba96265bb273af3e
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-09-09 16:17:42 +0700

    Модуль 5 финальное задание

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,57 +1,98 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Drawing;
 
 class MainClass
 {
     public static void Main(string[] args)
+
     {
-        (string Name, string Lastname, int Age, bool HasPet, string[] colors) Enteruser;
+        var User = GetData();
+        ShowData(User.Item1, User.Item2, User.Item3, User.Item4, User.Item5, User.Item6);
+
+     
+    static (string, string, int, bool, int, string[], int, string[]) GetData()
+    {
+        Console.WriteLine("Введите свое имя");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Введите свою фамилию");
+        string Surname = Console.ReadLine();
+        Console.WriteLine("Введите возраст пользователя цифрами");
+        int Age = Proverka();
+        Console.WriteLine("Есть ли у вас животные");
+        bool HavePet = Console.ReadLine() == "Да" ? true : false;
+        int Pets;
+        string[] PetNames;
+        if (HavePet)
         {
-            (string Name, string Lastname, int Age, bool HasPet, string[] colors) user;
-
-            Console.WriteLine("Введите свое имя");
-            user.Name = Console.ReadLine();
-
-            Console.WriteLine("Введите свою фамилию");
-            user.Lastname = Console.ReadLine();
-
-            bool validAge = false;
-            while (!validAge)
+            Console.WriteLine("Сколько у вас животных");
+            Pets = Proverka();
+            PetNames = new string[Pets];
+            for (int i = 0; i < Pets; i++)
             {
-                try
-                {
-                    Console.Write("Введите возраст: ");
-                    int Age = int.Parse(Console.ReadLine());
-                    validAge = true;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Ошибка: возраст должен быть целым числом.");
-                }
-
-                Console.WriteLine("Есть ли у вас животные? Да или нет");
-                var result = Console.ReadLine();
-                if (result == "Да")
-                {
-                    user.HasPet = true;
-                    Console.WriteLine("перечислите всех ваших домашних питомцев");
-
-                }
-                else
-                {
-                    user.HasPet = false;
-
-                    user.colors = new string[3];
-                    Console.WriteLine("Введите любимыe цвета");
-
-                    for (int i = 0; i < user.colors.Length; i++)
-                    {
-                        user.colors[i] = Console.ReadLine();
-
-                    }
-                }
-                Console.WriteLine($"Вы ввели: Имя - {user.Name},Фамилия - {user.Lastname}, Возраст - {int.MinValue}, Домашние питомцы - {user.HasPet}, Любимые цвета -{0}");
+                Console.WriteLine("Как зовут ваше животное");
+                PetNames[i] = Console.ReadLine();
             }
         }
+        else
+        {
+            Pets = 0;
+            PetNames = new string[1] { "none" };
+        }
+
+        Console.WriteLine("Сколько у вас любимых цветов?");
+        int Colors = Proverka();
+        string[] ColorsNames = new string[Colors];
+        for (int i = 0; i < Colors; i++)
+        {
+            Console.WriteLine("Какие любимые цвета?");
+            ColorsNames[i] = Console.ReadLine();
+        }
+
+        return (Name, Surname, Age, HavePet, Pets, PetNames, Colors, ColorsNames);
+    }
+
+    static void ShowData(string Name, string Surname, int Age, bool HavePet, int Pets, string[] PetNames)
+    {
+        Console.WriteLine(Name + " " + Surname);
+        Console.WriteLine(Age);
+        Console.WriteLine(HavePet);
+        Console.WriteLine(Pets);
+        foreach (string PetName in PetNames)
+        {
+            Console.WriteLine(PetName);
+        }
+    }
+
+    static int Proverka()
+    {
+        int Number = 0;
+        bool b = true;
+        int Age =0;
+        const int MinAge = 0;
+        const int MaxAge = 100;
+
+        while (b)
+        {
+            b = false;
+            string a = Console.ReadLine();
+            
+            try
+            {
+                Age = Int32.Parse(a);
+            }
+            catch
+            {
+                Console.WriteLine("Возоаст должен быть числом.");
+                b = true;
+            }
+
+            if (Age < MinAge || Age > MaxAge)
+            {
+                Console.WriteLine("Возоаст должен быть цифрами от 0 до 120");
+                b = true;
+            }
+        }
+        return Age;
     }
 }
